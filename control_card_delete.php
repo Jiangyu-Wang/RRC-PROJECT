8560fedad409fc9ef3f55009a169ac1f629f2753
JayusTree <jayustree@foxmail.com>
2021-04-08 03:52:14 -0500

    delet image from folder
    now images will be deleted from the folder when deleting a card.

--- control_card_delete.php
+++ control_card_delete.php
@@ -3,7 +3,10 @@
 $HSDB = require './lib/connect.php';
 
 $id = filter_input(INPUT_POST, 'id');
+$card = $HSDB->getCardById($id)[0];
 $HSDB -> deleteCard($id);
+
+unlink('./imgs/'.$card['img']);
 
 header('Location: ./view_forged_in_the_barrens.php');
 
