2d41fc9bb0102af2c867655d292b95837f305e18
JayusTree <jayustree@foxmail.com>
2021-04-11 04:27:10 -0500

    ajax get contents

--- view_forged_in_the_barrens.php
+++ view_forged_in_the_barrens.php
@@ -13,9 +13,15 @@ $heroClasses = $HSDB->getClasses();
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Forged In The Barrens</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
+
+<form>
+    <input type="text" id="keywords" name="keywords">
+    <button type="submit" id="search-button">FIND</button>
+</form>
 
 <div>
     <a href="./view_card_add.php">add</a>
@@ -36,11 +42,29 @@ $heroClasses = $HSDB->getClasses();
     <?php endforeach ?>
 </ul>
 
-<?php foreach ($cards as $index => $card): ?>
-    <a href="./view_card_edit.php?id=<?= $card['id'] ?>">
-        <img src="imgs/<?= $card['img'] ?>">
-    </a>
-<?php endforeach ?>
+<div id="card-wrap">
+    <?php foreach ($cards as $index => $card): ?>
+        <a href="./view_card_edit.php?id=<?= $card['id'] ?>">
+            <img src="imgs/<?= $card['img'] ?>">
+        </a>
+    <?php endforeach ?>
+</div>
+
+<script type="text/javascript">
+    $("#search-button").click((event) => {
+        event.preventDefault();
+        $("#card-wrap").html("");
+        let keywords = $("#keywords").val();
+
+        $.getJSON( "./control_card_search_api.php", { KeyWords: keywords } ).done((cards) => {
+            cards.forEach((card) => {
+                let $cardLink = $("<a href='./view_card_edit.php?id=" + card.id + "'></a>");
+                $cardLink.append("<img src='imgs/" + card.img + "'>");
+                $("#card-wrap").append($cardLink);
+            });
+        });
+    });
+</script>
 
 </body>
 </html>
